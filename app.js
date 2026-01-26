c50e857dd985bacf36e4824fbf19b11aa6af7b53
Seth Taylor <128197176+DevSethT@users.noreply.github.com>
2026-01-26 16:54:40 -0600

    inital commit and start to set up

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(console.error);
+
+const { PORT = 3001 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
