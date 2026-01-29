d8233ffdc0eecc2da2a5cffd4ce822eb9357ea7c
Seth Taylor <128197176+DevSethT@users.noreply.github.com>
2026-01-29 10:52:17 -0600

    complete api routes, error handling and likes

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -54,8 +54,54 @@ const deleteItem = (req, res) => {
     });
 };
 
+const likeItem = (req, res) => {
+  clothingItem
+    .findByIdAndUpdate(
+      req.params.itemId,
+      { $addToSet: { likes: req.user._id } },
+      { new: true }
+    )
+    .orFail()
+    .then((clothingItem) => res.send(clothingItem))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid item ID" });
+      } else if (err.name === "DocumentNotFoundError") {
+        return res.status(NOT_FOUND).send({ message: "Item not found" });
+      } else
+        return res
+          .status(SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
+    });
+};
+
+const dislikeItem = (req, res) => {
+  clothingItem
+    .findByIdAndUpdate(
+      req.params.itemId,
+      { $pull: { likes: req.user._id } },
+      { new: true }
+    )
+    .orFail()
+    .then((clothingItem) => res.send(clothingItem))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid item ID" });
+      } else if (err.name === "DocumentNotFoundError") {
+        return res.status(NOT_FOUND).send({ message: "Item not found" });
+      } else
+        return res
+          .status(SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
+    });
+};
+
 module.exports = {
   getItems,
   createItem,
   deleteItem,
+  likeItem,
+  dislikeItem,
 };
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -4,6 +4,8 @@ const {
   getItems,
   createItem,
   deleteItem,
+  likeItem,
+  dislikeItem,
 } = require("../controllers/clothingItems");
 
 clothingRouter.get("/", getItems);
@@ -11,5 +13,9 @@ clothingRouter.get("/", getItems);
 clothingRouter.post("/", createItem);
 
 clothingRouter.delete("/:itemId", deleteItem);
+
+clothingRouter.put("/:itemId/likes", likeItem);
+
+clothingRouter.delete("/:itemId/likes", dislikeItem);
 
 module.exports = clothingRouter;
