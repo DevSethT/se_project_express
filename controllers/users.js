39537a65417e2243e894ccb0c7061b3708fada2e
Seth Taylor <128197176+DevSethT@users.noreply.github.com>
2026-01-29 11:46:30 -0600

    removed un-necacery arrow functions

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,17 @@
 const User = require("../models/user");
 const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR } = require("../utils/errors");
 
-const getUsers = (req, res) => {
+const getUsers = (req, res) =>
   User.find({})
     .then((users) => res.send(users))
-    .catch(() => {
-      return res
+    .catch(() =>
+      res
         .status(SERVER_ERROR)
-        .send({ message: "An error has occurred on the server." });
-    });
-};
+        .send({ message: "An error has occurred on the server." })
+    );
 
-const getUser = (req, res) => {
-  const { userId } = req.params;
-
-  User.findById(userId)
+const getUser = (req, res) =>
+  User.findById(req.params.userId)
     .orFail()
     .then((user) => res.send(user))
     .catch((err) => {
@@ -28,23 +25,18 @@ const getUser = (req, res) => {
         .status(SERVER_ERROR)
         .send({ message: "An error has occurred on the server." });
     });
-};
 
-const createUser = (req, res) => {
-  const { name, avatar } = req.body;
-
-  User.create({ name, avatar })
+const createUser = (req, res) =>
+  User.create(req.body)
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === "ValidationError") {
         return res.status(BAD_REQUEST).send({ message: "Invalid data" });
       }
-
       return res
         .status(SERVER_ERROR)
         .send({ message: "An error has occurred on the server." });
     });
-};
 
 module.exports = {
   getUsers,
