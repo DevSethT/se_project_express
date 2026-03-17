e83d61462d750dc45bd3717cd0e0b6e6eaac18e8
Seth Taylor <128197176+DevSethT@users.noreply.github.com>
2026-03-17 17:57:05 -0500

    error.js handlers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const errorHandler = require("./middlewares/error-handler");
 
 const routes = require("./routes");
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+
+app.use(errorHandler);
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").catch(() => {});
 app.listen(PORT);
